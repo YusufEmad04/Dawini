91298cd254eee845b0ffbd5cf12bb854dec94be6
yusuf emad <yusufy2004@gmail.com>
2023-11-25 16:40:09 +0200

    revert commit

--- server_fast_api.py
+++ server_fast_api.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from agent_generator import *
 import time
 import asyncio
 import os
 import uvicorn
-
-from development import *
 
 app = FastAPI()
 
@@ -18,30 +16,5 @@ async def stream_response(data: dict):
 
     return StreamingResponse(run_agent(question, session_id), media_type='text/plain')
 
-@app.post("/drug-interactions")
-async def drug_interactions(drugs: dict):
-
-    try:
-        drugs = drugs["drugs"]
-        if not isinstance(drugs, list):
-            raise HTTPException(status_code=400, detail="Invalid payload. 'drugs' should be a list.")
-
-        interactions = describe_interactions(drugs)
-
-        if not interactions["pairs"]:
-
-            return {
-                "drug_interactions": False,
-                "explanation": "No interactions found"
-            }
-
-        explanation = explain_interactions(interactions)
-
-        return {
-            "drug_interactions": True,
-            "explanation": explanation
-        }
-    except:
-        raise HTTPException(status_code=400, detail="Invalid payload. 'drugs' should be a list.")
 
 uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8080")))
